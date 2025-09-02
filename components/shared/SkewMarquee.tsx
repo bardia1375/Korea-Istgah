a42d8615c19e753edf46b77bc8e1454f89b09d51
Bardia1375 <bardiashams1375@gmail.com>
2025-09-02 17:21:21 +0330

    remove

--- components/shared/SkewMarquee.tsx
+++ components/shared/SkewMarquee.tsx
@@ -12,9 +12,9 @@ const MARQUEE_IMAGES = [
   { id: 1, src: '/images/1156in1477-/1.webp' },
   { id: 2, src: '/images/1156in1477-/2.webp' },
   { id: 3, src: '/images/1156in1477-/3.webp' },
-  { id: 4, src: '/images/1156in1477-/golden-min.webp' },
-  { id: 5, src: '/images/1156in1477-/5.webp' },
-  { id: 6, src: '/images/1156in1477-/6.webp' },
+  // { id: 4, src: '/images/1156in1477-/golden-min.webp' },
+  { id: 4, src: '/images/1156in1477-/5.webp' },
+  { id: 5, src: '/images/1156in1477-/6.webp' },
 ]
 
 const SkewMarquee = () => {
