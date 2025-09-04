2200ec6783d246541cd99ce1ac4f62b060e6c58b
Bardia1375 <bardiashams1375@gmail.com>
2025-09-04 12:20:05 +0330

    Add footer change

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,9 +14,7 @@ const Footer = () => {
             <div className="mr-3 h-4 w-4 animate-pulse rounded-full bg-yellow-400"></div>
             <div className="relative overflow-hidden">
               <p className="rounded-md border-l-4 border-yellow-400 bg-white/10 px-4 py-1 text-lg font-medium text-white backdrop-blur-sm">
-                © 2025 <Link href="https://istgahmusical.com">Istgah Institute</Link>. All rights reserved. | Designed
-                by <a href="https://www.linkedin.com/in/seyedbardiashams/">Bardia</a> &{' '}
-                <a href="https://www.linkedin.com/in/farzad-taghizadeh-b16286175/">Farzad</a>
+                © 2025 <Link href="https://istgahmusical.com">Istgah Institute</Link>. All rights reserved. 
               </p>
               <div className="z-5 animate-shine absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-gradient-to-r from-transparent to-white opacity-20"></div>
             </div>
